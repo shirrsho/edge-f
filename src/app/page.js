6e0da6c3fedab9c43b365bc394f92b1a3e032f23
Shirsho-Lab <bsse1106@iit.du.ac.bd>
2023-08-21 14:09:35 +0600

    dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,175 @@
-import Image from 'next/image'
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col h-screen bg-gray-100">
+    <div className="bg-white text-white shadow w-full p-3 fixed flex items-center justify-between">
+        <div className="flex items-center">
+            <div className="flex items-center">
+                <img src="https://www.emprenderconactitud.com/img/POC%20WCS%20(1).png" alt="Logo" className="w-28 h-18 mr-2"/>
+                <h2 className="font-bold text-xl">Edge</h2>
+            </div>
+            <div className="md:hidden flex items-center">
+                <button id="menuBtn">
+                    <i className="fas fa-bars text-gray-500 text-lg"></i>
+                </button>
+            </div>
         </div>
-      </div>
+    </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <div className="flex-1 flex flex-wrap mt-20">
+        <div className="p-2 bg-white w-full md:w-60 flex flex-col md:flex" id="sideNav">
+            <nav className="fixed">
+                <a className="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:text-gray-800" href="#">
+                    <i className="fas fa-home mr-2"></i>New Agreement
+                </a>
+                <a className="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:text-gray-800" href="#">
+                    <i className="fas fa-file-alt mr-2"></i>New Batch
+                </a>
+                <a className="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:text-gray-800" href="#">
+                    <i className="fas fa-users mr-2"></i>Update
+                </a>
+            </nav>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="flex-1 p-4 w-full md:w-1/2">
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+            <div className="mt-8 flex flex-wrap space-x-0 space-y-2 md:space-x-4 md:space-y-0">
+                <div className="flex-1 bg-white p-4 shadow rounded-lg md:w-1/2">
+                    <h2 className="text-gray-500 text-lg font-semibold pb-1">Companies</h2>
+                    <div className="my-1"></div>
+                    <div className="bg-gradient-to-r from-cyan-300 to-gray-800 h-px mb-6"></div>
+                    <div className="chart-container relative h-38 w-full">
+                        <canvas id="usersChart"></canvas>
+                    </div>
+                </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+                <div className="flex-1 bg-white p-4 shadow rounded-lg md:w-1/2">
+                    <h2 className="text-gray-500 text-lg font-semibold pb-1">Trained</h2>
+                    <div className="my-1"></div>
+                    <div className="bg-gradient-to-r from-cyan-300 to-gray-800 h-px mb-6"></div>
+                    <div className="chart-container" style={{position: 'relative', height:'150px', width:'100%'}}>
+                        <canvas id="commercesChart"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div className="relative max-w-md w-full mt-10">
+                  <div className="absolute top-1 left-2 inline-flex items-center p-2">
+                      <i className="fas fa-search text-gray-400"></i>
+                  </div>
+                <input className="w-full h-10 pl-10 pr-4 py-1 text-base placeholder-gray-500 border rounded-full focus:shadow-outline" type="search" placeholder="Search..."/>
+              </div>
+            <div className="mt-8 bg-white p-4 shadow rounded-lg">
+                <h2 className="text-gray-500 text-lg font-semibold pb-4">Due Payment</h2>
+                <div className="my-1"></div>
+                <div className="bg-gradient-to-r from-cyan-300 to-gray-800 h-px mb-6"></div>
+                <table className="w-full table-auto text-sm">
+                    <thead>
+                        <tr className="text-sm leading-normal">
+                            <th className="py-4 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">Sl</th>
+                            <th className="py-4 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">Company</th>
+                            <th className="py-4 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">Batch</th>
+                            <th className="py-4 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="hover:bg-grey-lighter">
+                            <td className="py-4 px-4 border-b border-grey-light">1</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Company</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Batch-6789</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Submitted</td>
+                        </tr>
+                        <tr className="hover:bg-grey-lighter">
+                            <td className="py-4 px-4 border-b border-grey-light">2</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Company</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Batch-6789</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Submitted</td>
+                        </tr>
+                        <tr className="hover:bg-grey-lighter">
+                            <td className="py-4 px-4 border-b border-grey-light">3</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Company</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Batch-6789</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Submitted</td>
+                        </tr>
+                        <tr className="hover:bg-grey-lighter">
+                            <td className="py-4 px-4 border-b border-grey-light">4</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Company</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Batch-6789</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Submitted</td>
+                        </tr>
+                        <tr className="hover:bg-grey-lighter">
+                            <td className="py-4 px-4 border-b border-grey-light">5</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Company</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Batch-6789</td>
+                            <td className="py-4 px-4 border-b border-grey-light">Submitted</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div className="text-right mt-4">
+                    <button className="bg-gray-800 hover:bg-cyan-600 text-white font-semibold py-2 px-4 rounded">
+                        More
+                    </button>
+                </div>
+            </div>
+
+            <div className="mt-8 bg-white p-4 shadow rounded-lg">
+                <div className="bg-white p-4 rounded-md mt-4">
+                    <h2 className="text-gray-500 text-lg font-semibold pb-4">Upcoming Deadlines</h2>
+                    <div className="my-1"></div>
+                    <div className="bg-gradient-to-r from-cyan-300 to-gray-800 h-px mb-6"></div>
+                    <table className="w-full table-auto text-sm">
+                        <thead>
+                            <tr className="text-sm leading-normal">
+                                <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">Sl</th>
+                                <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">Company</th>
+                                <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">Batch</th>
+                                <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">Event</th>
+                                <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">Deadline</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="hover:bg-grey-lighter">
+                                <td className="py-2 px-4 border-b border-grey-light">1</td>
+                                <td className="py-2 px-4 border-b border-grey-light">Company</td>
+                                <td className="py-2 px-4 border-b border-grey-light">Batch-24323</td>
+                                <td className="py-2 px-4 border-b border-grey-light">Mid Evaluation</td>
+                                <td className="py-2 px-4 border-b border-grey-light">02/09/2024</td>
+                            </tr>
+                        <tr className="hover:bg-grey-lighter">
+                                <td className="py-2 px-4 border-b border-grey-light">2</td>
+                                <td className="py-2 px-4 border-b border-grey-light">RealTech</td>
+                                <td className="py-2 px-4 border-b border-grey-light">Batch-24323</td>
+                                <td className="py-2 px-4 border-b border-grey-light">Final Evaluation</td>
+                                <td className="py-2 px-4 border-b border-grey-light">04/09/2024</td>
+                        </tr>
+                        <tr className="hover:bg-grey-lighter">
+                                <td className="py-2 px-4 border-b border-grey-light">3</td>
+                                <td className="py-2 px-4 border-b border-grey-light">Company</td>
+                                <td className="py-2 px-4 border-b border-grey-light">Batch-24323</td>
+                                <td className="py-2 px-4 border-b border-grey-light">Mid Evaluation</td>
+                                <td className="py-2 px-4 border-b border-grey-light">02/09/2024</td>
+                        </tr>
+                        <tr className="hover:bg-grey-lighter">
+                                <td className="py-2 px-4 border-b border-grey-light">4</td>
+                                <td className="py-2 px-4 border-b border-grey-light">Company</td>
+                                <td className="py-2 px-4 border-b border-grey-light">Batch-24323</td>
+                                <td className="py-2 px-4 border-b border-grey-light">Mid Evaluation</td>
+                                <td className="py-2 px-4 border-b border-grey-light">02/09/2024</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div className="text-right mt-4">
+                                                <div className="text-right mt-4">
+                            <button className="bg-gray-800 hover:bg-cyan-600 text-white font-semibold py-2 px-4 rounded">
+                                More
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
   )
 }
